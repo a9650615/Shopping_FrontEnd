a36cce60a6f6e306fa7f2667f2b3401bf4fad835
菜鳥祐 <a9650615@gmail.com>
2017-11-29 23:57:18 +0800

    start structer

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,31 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
-import { initStore, startClock, addCount, serverRenderClock } from '../store'
+import { initStore } from '../store'
 import withRedux from 'next-redux-wrapper'
-import Page from '../components/Page'
 
 class Counter extends React.Component {
   static getInitialProps ({ store, isServer }) {
-    store.dispatch(serverRenderClock(isServer))
-    store.dispatch(addCount())
+    // store.dispatch(serverRenderClock(isServer))
 
-    return { isServer }
+    return { }
   }
 
   componentDidMount () {
-    this.timer = this.props.startClock()
   }
 
   componentWillUnmount () {
-    clearInterval(this.timer)
   }
 
   render () {
     return (
-      <Page title='Index Page' linkTo='/other' />
+      <div>index</div>
     )
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    addCount: bindActionCreators(addCount, dispatch),
-    startClock: bindActionCreators(startClock, dispatch)
+    // startClock: bindActionCreators(startClock, dispatch)
   }
 }
 
--- pages/other.js
+++ pages/other.js
@@ -2,34 +2,28 @@ import React from 'react'
 import { bindActionCreators } from 'redux'
 import { initStore, startClock, addCount, serverRenderClock } from '../store'
 import withRedux from 'next-redux-wrapper'
-import Page from '../components/Page'
 
 class Counter extends React.Component {
   static getInitialProps ({ store, isServer }) {
-    store.dispatch(serverRenderClock(isServer))
-    store.dispatch(addCount())
-    return { isServer }
+    
+    return { }
   }
 
   componentDidMount () {
-    this.timer = this.props.startClock()
   }
 
   componentWillUnmount () {
-    clearInterval(this.timer)
   }
 
   render () {
     return (
-      <Page title='Other Page' linkTo='/' />
+      <div>other</div>
     )
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    addCount: bindActionCreators(addCount, dispatch),
-    startClock: bindActionCreators(startClock, dispatch)
   }
 }
 
